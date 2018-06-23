deec793f10506f81b2930255e166ea60c0b84f5b
Muhammad Sajad <sajadkhan71@yahoo.com>
2018-06-23 08:02:14 +0500

    Starting from Set
    Intial commit on starting Graphic Set. Code from Set.

---
+++ Graphic Set/Set/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  Set
+//
+//  Created by Sajad on 6/23/18.
+//  Copyright © 2018 TPLHolding. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+
+    
+    
+    override func draw(_ rect: CGRect) {
+        
+    }
+    
+
+}
---
+++ Graphic Set/Set/Model/Card.swift
@@ -0,0 +1,54 @@
+//
+//  Card.swift
+//  Set
+//
+//  Created by Sajad on 6/21/18.
+//  Copyright © 2018 TPLHolding. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    enum Shade: String {
+        case solid
+        case striped
+        case open
+        static let allCases: [Shade] = [.solid, .striped, .open]
+    }
+    
+    enum Symbol: String {
+        case diamond
+        case squiggle
+        case oval
+        static let allCases: [Symbol] = [.diamond, .squiggle, .oval]
+    }
+    
+    enum Color: String {
+        case red
+        case green
+        case purple
+        static let allCases: [Color] = [.red, .green, .purple]
+    }
+    
+    enum Number: Int {
+        case one = 1
+        case two
+        case three
+        static let allCases: [Number] = [.one, .two, .three]
+    }
+    
+    let number: Number
+    let shading: Shade
+    let symbol: Symbol
+    let color: Color
+}
+
+extension Card: Equatable {
+    static func ==(lhs: Card, rhs: Card) -> Bool {
+        return lhs.color == rhs.color &&
+            lhs.number == rhs.number &&
+            lhs.shading == rhs.shading &&
+            lhs.symbol == rhs.symbol
+    }
+}
+
